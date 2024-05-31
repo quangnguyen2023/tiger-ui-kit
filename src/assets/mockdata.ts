1f2fa2f980a194f4370ef7c0009c212207daa3e3
Quang2023 <nmquang2012@gmail.com>
2024-05-31 15:50:37 +0700

    feat (Weather Forecast): add mockdata for hourly forecast

---
+++ src/assets/mockdata.ts
@@ -0,0 +1,8 @@
+export const hourlyForecast = [
+  { hour: 12, icon: '09d', temperature: 36 },
+  { hour: 13, icon: '10d', temperature: 35 },
+  { hour: 14, icon: '13d', temperature: 34 },
+  { hour: 15, icon: '02d', temperature: 32 },
+  { hour: 16, icon: '04d', temperature: 38 },
+  { hour: 17, icon: '11d', temperature: 38 },
+];
