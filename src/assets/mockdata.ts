6cba266fd9ced48f491e3ab8cadc140f28f2b898
Quang2023 <nmquang2012@gmail.com>
2024-06-01 23:52:14 +0700

    feat (Weather Forecast): Create mockdata for daily forecast

--- src/assets/mockdata.ts
+++ src/assets/mockdata.ts
@@ -6,3 +6,11 @@ export const hourlyForecast = [
   { hour: 16, icon: '04d', temperature: 38 },
   { hour: 17, icon: '11d', temperature: 38 },
 ];
+
+export const dailyForecast = [
+  { dayOfWeek: 'Mon', icon: '01d', temperature: { max: 36, min: 27, temp: 30 } },
+  { dayOfWeek: 'Tue', icon: '02d', temperature: { max: 33, min: 28, temp: 30 } },
+  { dayOfWeek: 'Wed', icon: '03d', temperature: { max: 36, min: 27, temp: 30 } },
+  { dayOfWeek: 'Thu', icon: '04d', temperature: { max: 34, min: 25, temp: 30 } },
+  { dayOfWeek: 'Fri', icon: '09d', temperature: { max: 32, min: 24, temp: 30 } },
+];
