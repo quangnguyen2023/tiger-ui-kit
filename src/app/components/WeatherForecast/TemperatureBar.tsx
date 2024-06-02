954e818d2994b78c6b385f324eb26f5eb0743881
Quang2023 <nmquang2012@gmail.com>
2024-06-02 11:22:11 +0700

    feat (Weather Forecast): Updated temperature bar length calculation

--- src/app/components/WeatherForecast/TemperatureBar.tsx
+++ src/app/components/WeatherForecast/TemperatureBar.tsx
@@ -1,25 +1,32 @@
 type TemperatureBarProps = {
+  upperLimit: number;
+  lowerLimit: number;
   maxTemp: number;
   minTemp: number;
-  temp: number;
   classNames?: string;
 };
 
 export default function TemperatureBar({
-  temp,
+  upperLimit,
+  lowerLimit,
   maxTemp,
   minTemp,
   classNames,
 }: TemperatureBarProps) {
-  const tempBarLengthPercent = `${((temp - minTemp) / (maxTemp - minTemp)) * 100}%`;
+  const tempBarLengthPercent = `${((maxTemp - minTemp) / (upperLimit - lowerLimit)) * 100}%`;
+  const distanceToLowerLimit = `${((minTemp - lowerLimit) / (upperLimit - lowerLimit)) * 100}%`;
 
   return (
-    <div className={`${classNames} flex items-center gap-3`}>
+    <div className={`${classNames} flex items-center gap-4`}>
       <div className="opacity-70">{minTemp}°</div>
-      <div className="relative w-full h-1 bg-gray-200 rounded-full">
+      <div className="relative w-full">
+        <div className="absolute -translate-y-1/2 w-full h-[5px] bg-black opacity-[0.09] rounded-full" />
         <div
-          className="absolute h-1 bg-red-500 rounded-full"
-          style={{ width: `${tempBarLengthPercent}` }}
+          className="absolute -translate-y-1/2 h-[5px] bg-gradient-to-r from-orange-300 to-red-500 rounded-full"
+          style={{
+            width: `${tempBarLengthPercent}`,
+            left: `${distanceToLowerLimit}`,
+          }}
         />
       </div>
       <div>{maxTemp}°</div>
