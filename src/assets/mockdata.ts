770a1eb9bc759fde8c84fb2a9068098cb1135ea4
Quang2023 <nmquang2012@gmail.com>
2024-06-03 23:59:39 +0700

    style (Weather Forecast): Update css

--- src/assets/mockdata.ts
+++ src/assets/mockdata.ts
@@ -16,9 +16,9 @@ export const hourlyForecast = [
 
 // prettier-ignore
 export const dailyForecast = [
-  { dayOfWeek: 'Mon', icon: '01d', temperature: { max: 36, min: 27, lowerLimit: 24, upperLimit: 37 } },
-  { dayOfWeek: 'Tue', icon: '02d', temperature: { max: 33, min: 25, lowerLimit: 24, upperLimit: 37 } },
-  { dayOfWeek: 'Wed', icon: '03d', temperature: { max: 36, min: 27, lowerLimit: 24, upperLimit: 37 } },
-  { dayOfWeek: 'Thu', icon: '04d', temperature: { max: 34, min: 25, lowerLimit: 24, upperLimit: 37 } },
-  { dayOfWeek: 'Fri', icon: '09d', temperature: { max: 32, min: 24, lowerLimit: 24, upperLimit: 37 } },
+  { dayOfWeek: 'Mon', icon: '01d', temperature: { max: 36, min: 24, lowerLimit: 24, upperLimit: 36 } },
+  { dayOfWeek: 'Tue', icon: '02d', temperature: { max: 33, min: 25, lowerLimit: 24, upperLimit: 36 } },
+  { dayOfWeek: 'Wed', icon: '03d', temperature: { max: 36, min: 27, lowerLimit: 24, upperLimit: 36 } },
+  { dayOfWeek: 'Thu', icon: '04d', temperature: { max: 34, min: 25, lowerLimit: 24, upperLimit: 36 } },
+  { dayOfWeek: 'Fri', icon: '09d', temperature: { max: 32, min: 24, lowerLimit: 24, upperLimit: 36 } },
 ];
