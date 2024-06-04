47b565e66e12380868064ee4b8c1bcce8f357b2d
Quang2023 <nmquang2012@gmail.com>
2024-06-04 18:17:45 +0700

    feat (Calendar): Add story for Calendar component

---
+++ src/app/components/Calendar/Calendar.stories.ts
@@ -0,0 +1,14 @@
+import { Meta, StoryObj } from '@storybook/react';
+import Calendar from './Calendar';
+
+const meta = {
+  title: 'UI/Calendar',
+  component: Calendar,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Calendar>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
