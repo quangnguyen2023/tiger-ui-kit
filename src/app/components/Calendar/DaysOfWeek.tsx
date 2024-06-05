009638e32edffcbd7ba2a8ecf93e21a34028b56f
Quang2023 <nmquang2012@gmail.com>
2024-06-05 19:58:28 +0700

    feat (Calendar): Allow to set the first day of week as Monday or Sunday

--- src/app/components/Calendar/DaysOfWeek.tsx
+++ src/app/components/Calendar/DaysOfWeek.tsx
@@ -1,14 +1,42 @@
-const daysOfWeek = ['M', 'T', 'W', 'T', 'F', 'S', 'S'];
+import { useEffect, useState } from 'react';
+import { firstDayOfWeekType } from './Calendar';
 
-export default function DaysOfWeek() {
+type DaysOfWeekProps = {
+  firstDayOfWeek: firstDayOfWeekType;
+};
+
+export default function DaysOfWeek({ firstDayOfWeek }: DaysOfWeekProps) {
+  const [daysOfWeek, setDaysOfWeek] = useState([
+    { value: 'Mon', isWeekend: false },
+    { value: 'Tue', isWeekend: false },
+    { value: 'Wed', isWeekend: false },
+    { value: 'Thu', isWeekend: false },
+    { value: 'Fri', isWeekend: false },
+    { value: 'Sat', isWeekend: true },
+    { value: 'Sun', isWeekend: true },
+  ]);
+
+  useEffect(() => {
+    if (firstDayOfWeek === 'Sunday' && daysOfWeek[0].value !== 'Sun') {
+      setDaysOfWeek((prev: any) => {
+        const sunday = prev.find((day: any) => day.value === 'Sun');
+        const otherDays = prev.filter((day: any) => day.value !== 'Sun');
+        return [sunday, ...otherDays];
+      });
+    } else if (firstDayOfWeek === 'Monday' && daysOfWeek[0].value !== 'Mon') {
+      setDaysOfWeek((prev: any) => {
+        const sunday = prev.find((day: any) => day.value === 'Sun');
+        const otherDays = prev.filter((day: any) => day.value !== 'Sun');
+        return [...otherDays, sunday];
+      });
+    }
+  }, [firstDayOfWeek]);
+
   return (
     <div className="grid grid-cols-7 gap-1 mt-2 font-semibold">
       {daysOfWeek.map((day, index) => (
-        <div
-          key={index}
-          className={`text-center ${index === daysOfWeek.length - 2 || index === daysOfWeek.length - 1 ? 'text-[#9b9b9b]' : ''}`}
-        >
-          {day}
+        <div key={index} className={`text-center ${day.isWeekend ? 'text-[#9b9b9b]' : ''}`}>
+          {day.value.slice(0, 1)}
         </div>
       ))}
     </div>
