04d9a25a54365d1c2976877a316cc286b9ed1440
Quang2023 <nmquang2012@gmail.com>
2024-06-05 14:56:31 +0700

    feat (Calendar): Add lunar calendar

--- src/app/components/Calendar/DaysOfMonth.tsx
+++ src/app/components/Calendar/DaysOfMonth.tsx
@@ -2,22 +2,40 @@ import { DayOfMonthType } from './Calendar';
 
 type DaysOfMonthProps = {
   daysOfMonth: DayOfMonthType[];
+  enableLunarCalendar?: boolean;
 };
 
-export default function DaysOfMonth({ daysOfMonth }: DaysOfMonthProps) {
+export default function DaysOfMonth({ daysOfMonth, enableLunarCalendar }: DaysOfMonthProps) {
   return (
-    <div className="grid grid-cols-7 mt-2 gap-1 font-semibold">
-      {daysOfMonth.map((day) => (
+    <div className="grid grid-cols-7 mt-2 gap-1 gap-y-2">
+      {daysOfMonth.map((day, index) => (
         <div
-          key={day.value}
+          key={index}
           className={`
+            relative 
             ${day.isCurrentDay ? 'bg-[#f64338] rounded-full' : ''} 
-            ${day.isWeekendDay ? 'text-[#9b9b9b]' : ''} relative p-3.5
+            ${day.isWeekendDay ? 'text-[#9b9b9b]' : ''} 
+            ${enableLunarCalendar ? 'p-5 rounded-md' : 'p-3.5'} 
           `}
         >
-          <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-            {day.value}
-          </span>
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center">
+            <span className="">{day.value}</span>
+
+            {enableLunarCalendar && day.value && (
+              <span
+                className={`
+                  -mt-2 ml-4 text-slate-200 text-[0.55rem] 
+                  ${day.isWeekendDay ? '!text-[#9b9b9b]' : ''}
+                  ${day.lunarValue?.date === 1 && !day.isCurrentDay ? 'text-red-500' : ''}
+                  ${day.lunarValue?.date === 1 && day.isCurrentDay ? 'text-white' : ''}
+                `}
+              >
+                {day.lunarValue?.date === 1
+                  ? `${day.lunarValue?.date}/${day.lunarValue?.month}`
+                  : day.lunarValue?.date}
+              </span>
+            )}
+          </div>
         </div>
       ))}
     </div>
