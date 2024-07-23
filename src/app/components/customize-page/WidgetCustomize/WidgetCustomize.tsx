95888c661cbb6f2c6ce3bec3ffde0b1b0088c4e5
Quang2023 <nmquang2012@gmail.com>
2024-07-23 23:32:06 +0700

    feat: Save new customize props to cookie

--- src/app/components/customize-page/WidgetCustomize/WidgetCustomize.tsx
+++ src/app/components/customize-page/WidgetCustomize/WidgetCustomize.tsx
@@ -18,52 +18,60 @@ export type CustomizeItem = {
 type WidgetCustomizeProps = {
   customizeItems: CustomizeItem[];
   handleChange: (newProps: CustomizeProps) => void;
+  handleSave: () => void;
 };
 
-export default function WidgetCustomize({ customizeItems, handleChange }: WidgetCustomizeProps) {
-  const getCustomizeItem = (item: CustomizeItem) => {
-    switch (item.type) {
-      case CustomizeItemType.COLOR:
-        return (
-          <ColorPicker
-            customValue={item.defaultValue as string}
-            onChangeColor={(color) => {
-              handleChange({ [item.fieldName]: color });
-            }}
-          />
-        );
-      case CustomizeItemType.SWITCHER:
-        return (
-          <Switcher
-            value={item.defaultValue}
-            options={item?.options || []}
-            onSwitch={(value) => handleChange({ [item.fieldName]: value })}
-          />
-        );
-      case CustomizeItemType.INPUT:
-        return (
-          <Input
-            value={item.defaultValue}
-            onChange={(value) => {
-              handleChange({ [item.fieldName]: value });
-            }}
-          />
-        );
-      case CustomizeItemType.NUMBER:
-        return (
-          <Input
-            type="number"
-            value={item.defaultValue}
-            onChange={(value) => {
-              handleChange({ [item.fieldName]: value });
-            }}
-          />
-        );
-      default:
-        return null;
-    }
-  };
+const getCustomizeItem = (
+  item: CustomizeItem,
+  handleChange: (newProps: CustomizeProps) => void
+) => {
+  switch (item.type) {
+    case CustomizeItemType.COLOR:
+      return (
+        <ColorPicker
+          customValue={item.defaultValue as string}
+          onChangeColor={(color) => {
+            handleChange({ [item.fieldName]: color });
+          }}
+        />
+      );
+    case CustomizeItemType.SWITCHER:
+      return (
+        <Switcher
+          value={item.defaultValue}
+          options={item?.options || []}
+          onSwitch={(value) => handleChange({ [item.fieldName]: value })}
+        />
+      );
+    case CustomizeItemType.INPUT:
+      return (
+        <Input
+          value={item.defaultValue}
+          onChange={(value) => {
+            handleChange({ [item.fieldName]: value });
+          }}
+        />
+      );
+    case CustomizeItemType.NUMBER:
+      return (
+        <Input
+          type="number"
+          value={item.defaultValue}
+          onChange={(value) => {
+            handleChange({ [item.fieldName]: value });
+          }}
+        />
+      );
+    default:
+      return null;
+  }
+};
 
+export default function WidgetCustomize({
+  customizeItems,
+  handleChange,
+  handleSave,
+}: WidgetCustomizeProps) {
   return (
     <>
       <div className="text-xl font-semibold mb-6"> Customize </div>
@@ -71,9 +79,20 @@ export default function WidgetCustomize({ customizeItems, handleChange }: Widget
         {customizeItems.map((item, index) => (
           <div className="flex flex-col gap-3" key={index}>
             <label className="text-sm font-semibold"> {item.title} </label>
-            {getCustomizeItem(item)}
+            {getCustomizeItem(item, handleChange)}
           </div>
         ))}
+      </div>
+
+      <div className="flex justify-end my-5">
+        <button
+          className="
+            px-4 py-2 text-sm font-medium bg-blue-400 text-white rounded-md transition hover:bg-blue-500 
+            active:bg-blue-600"
+          onClick={handleSave}
+        >
+          Save Changes
+        </button>
       </div>
     </>
   );
