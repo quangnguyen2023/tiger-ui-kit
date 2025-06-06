d8e20a83729d6ce5f8fcba721f8b2e5aa9c8ba81
Quang <nmquang2012@gmail.com>
2025-06-06 17:06:33 +0700

    fix: The error occurs when cookies from next/head return a promise

--- api/widget.ts
+++ api/widget.ts
@@ -3,27 +3,25 @@
 import { Widget } from '@/types/widget';
 import { cookies } from 'next/headers';
 
-export const getExistingWidgets: () => Record<string, Widget> = () => {
-  const widgetsCookie = cookies().get('widgets');
+export const getExistingWidgets: () => Promise<Record<string, Widget>> = async () => {
+  const widgetsCookie = (await cookies()).get('widgets');
   return widgetsCookie ? JSON.parse(widgetsCookie.value) : {};
 };
 
 export const apiCreateWidget = async (id: string, newWidget: Widget) => {
   const widgets = await getExistingWidgets();
-  console.log('existing widgets:', widgets);
   widgets[id] = newWidget;
-  console.log('updated widgets:', widgets);
-  cookies().set('widgets', JSON.stringify(widgets));
+  (await cookies()).set('widgets', JSON.stringify(widgets));
 };
 
 export const apiSaveWidget = async (id: string, updatedWidget: Widget) => {
   const widgets = await getExistingWidgets();
   widgets[id] = updatedWidget;
-  cookies().set('widgets', JSON.stringify(widgets));
+  (await cookies()).set('widgets', JSON.stringify(widgets));
 };
 
 export const apiDeleteWidget = async (id: string) => {
   const widgets = await getExistingWidgets();
   delete widgets[id];
-  cookies().set('widgets', JSON.stringify(widgets));
+  (await cookies()).set('widgets', JSON.stringify(widgets));
 };
