fbe997c760cbc013b553d9224c95f5c2f6bf8a21
Quang <nmquang2012@gmail.com>
2025-06-08 21:48:47 +0700

    feat: Add loading state to Button component with spinner indicator

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,8 +1,9 @@
 import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
+import { Slot, Slottable } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
+import { Loader } from "lucide-react"
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap cursor-pointer rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
@@ -39,11 +40,15 @@ function Button({
   className,
   variant,
   size,
+  children,
+  disabled,
   asChild = false,
+  loading = false,
   ...props
 }: React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean
+    loading?: boolean
   }) {
   const Comp = asChild ? Slot : "button"
 
@@ -51,9 +56,13 @@ function Button({
     <Comp
       data-slot="button"
       className={cn(buttonVariants({ variant, size, className }))}
+      disabled={disabled || loading}
       {...props}
-    />
-  )
+    >
+      {loading && <Loader className="animate-spin" />}
+      {!loading && <Slottable>{children}</Slottable>}
+    </Comp>
+  );
 }
 
 export { Button, buttonVariants }
