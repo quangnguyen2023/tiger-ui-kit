7005d4d05d29f9c14f4cf25a935988b6bbb70db0
Quang <nmquang2012@gmail.com>
2025-06-12 00:02:40 +0700

    config: Update next config which allow embeding link into Notion

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,19 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/embed/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: 'frame-ancestors https://www.notion.so',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
