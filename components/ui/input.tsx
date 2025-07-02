881f1d6604e6d2a45c77fbbe6acfda3a291bef59
Quang <nmquang2012@gmail.com>
2025-07-02 19:23:50 +0700

    feat(shadcn/input): add startIcon support to Input component

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,21 +1,39 @@
-import * as React from "react"
+import * as React from 'react';
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils';
+import { LucideIcon } from 'lucide-react';
 
-function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+function Input({
+  className,
+  type,
+  startIcon,
+  ...props
+}: React.ComponentProps<'input'> & {
+  startIcon?: LucideIcon;
+}) {
+  const StartIcon = startIcon;
+
   return (
-    <input
-      type={type}
-      data-slot="input"
-      className={cn(
-        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-        className
+    <div className="w-full relative">
+      {StartIcon && (
+        <div className="absolute left-3 top-1/2 transform -translate-y-1/2">
+          <StartIcon size={18} className="text-muted-foreground" />
+        </div>
       )}
-      {...props}
-    />
-  )
+      <input
+        type={type}
+        data-slot="input"
+        className={cn(
+          'file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+          'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
+          'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
+          startIcon ? 'pl-10' : '',
+          className
+        )}
+        {...props}
+      />
+    </div>
+  );
 }
 
-export { Input }
+export { Input };
