53411173af542f24345a1a6e7bf82e7d2f658b64
Quang <nmquang2012@gmail.com>
2025-09-12 12:38:05 +0700

    fix(NextAuth, axios): update environment variable name of base URL

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getSession } from 'next-auth/react';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+const API_BASE_URL = process.env.NEXTAUTH_URL;
 
 const createAxiosInstance = (baseURL: string) => {
   const apiClient = axios.create({
