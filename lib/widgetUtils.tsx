27efdf7cac115f5c3dde948be68bfaec3d8db73c
Quang <nmquang2012@gmail.com>
2025-10-05 23:46:34 +0700

    perf(widgetUtils): refactor renderWidgetComponent to use useMemo for optimization

--- lib/widgetUtils.tsx
+++ lib/widgetUtils.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import AnalogClock from '@/components/widgets/AnalogClock';
 import Calendar from '@/components/widgets/Calendar';
 import DigitalClock from '@/components/widgets/DigitalClock';
@@ -29,18 +30,21 @@ export const renderWidgetComponent = (
   widgetType: WidgetType,
   customValues?: Record<string, any>,
 ) => {
-  switch (widgetType) {
-    case WidgetType.ANALOG_CLOCK:
-      return <AnalogClock {...customValues} />;
-    case WidgetType.DIGITAL_CLOCK:
-      return <DigitalClock {...customValues} />;
-    case WidgetType.WORLD_CLOCK:
-      return <WorldClock {...customValues} />;
-    case WidgetType.WEATHER_FORECAST:
-      return <WeatherForecast {...customValues} />;
-    case WidgetType.CALENDAR:
-      return <Calendar {...customValues} />;
-    default:
-      return <AnalogClock {...customValues} />;
-  }
+  // Memoize component để tránh re-render không cần thiết
+  return useMemo(() => {
+    switch (widgetType) {
+      case WidgetType.ANALOG_CLOCK:
+        return <AnalogClock {...customValues} />;
+      case WidgetType.DIGITAL_CLOCK:
+        return <DigitalClock {...customValues} />;
+      case WidgetType.WORLD_CLOCK:
+        return <WorldClock {...customValues} />;
+      case WidgetType.WEATHER_FORECAST:
+        return <WeatherForecast {...customValues} />;
+      case WidgetType.CALENDAR:
+        return <Calendar {...customValues} />;
+      default:
+        return <AnalogClock {...customValues} />;
+    }
+  }, [widgetType, customValues]); // Chỉ re-render khi widgetType hoặc customValues thay đổi
 };
